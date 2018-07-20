7a181c4e3ae8298e994ad122eafea042ac247e51
b1tlite <b1tlite@hotmail.com>
2018-07-20 15:10:53 +0300

    Another loop tests v.2

--- 4.py
+++ 4.py
@@ -7,13 +7,13 @@ import os
 
 class goo():
 	def __init__(self):
-		os.system('cls')
+		#os.system('cls')
 		self.g = 13
 		self.stop = 0
 		self.loop = asyncio.get_event_loop()
 		#self.loop2= asyncio.get_event_loop()
-		tasks = [self.loop.create_task(self.fo()), self.loop.create_task(self.bar())]
-		wait_tasks = asyncio.wait(tasks)
+		self.tasks = [self.loop.create_task(self.fo()), self.loop.create_task(self.bar())]
+		wait_tasks = asyncio.wait(self.tasks)
 		self.loop.run_until_complete(wait_tasks)
 		self.loop.close()
 
@@ -30,7 +30,7 @@ class goo():
 		print('GGWP: Start')
 		for x in range(1,10):
 			print('GGWP: '+ str(x))
-			await asyncio.sleep(1)
+			await asyncio.sleep(2)
 		# while True:
 		# 	if self.stop == 1:
 		# 		break
@@ -39,18 +39,25 @@ class goo():
 	async def bar(self):
 		print('BAR: Running in BAR')
 		#await self.start_l2()
-		asyncio.ensure_future(self.gg_wp(),loop = self.loop)
+		#asyncio.ensure_future(self.gg_wp(),loop = self.loop)
 		await asyncio.sleep(4)
 		self.g = 0
-		self.stop = 1
 		print('BAR: G=0')
+		task = self.loop.create_task(self.gg_wp())
+		print('BAR: TASK')
+		self.tasks.append(task)
+		print('BAR: append')
+		asyncio.wait(self.tasks)
+		print('BAR: await')
+		self.stop = 1
+		
 		
 		# await self.gg_wp()
 		print('BAR: End')
 
 	async def start_l2(self):
 		print('ST_L2: Start')
-		#asyncio.ensure_future(self.gg_wp(),loop = self.loop)
+		#self.loop.create_task(self.gg_wp())
 		#wtask = asyncio.wait(task)
 		print('ST_L2: ggwp')
 		#self.loop2.run_until_complete(wtask)
